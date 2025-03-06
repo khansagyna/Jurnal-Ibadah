d47327b205049e2db7d10302c48fa52345cf7acd
khansagyna <amubafly11@gmail.com>
2025-03-06 23:20:55 +0700

    commit4

--- login/login_page.php
+++ login/login_page.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Jurnal Ibadah</title>
+    <title>Login | Jurnal-Ibadah</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="flex items-center justify-center min-h-screen bg-gray-100">
